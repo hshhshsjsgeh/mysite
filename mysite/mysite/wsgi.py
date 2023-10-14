0d764e4c2deb23c3927730e1ae05570c5e181949
يوسف <89800072+hshhshsjsgeh@users.noreply.github.com>
2023-10-14 12:16:24 +0300

    tst

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+
+# python manage.py runserver
+# python manage.py runserver 8080
+# python manage.py runserver 0.0.0.0:8000
+# python manage.py startapp polls
