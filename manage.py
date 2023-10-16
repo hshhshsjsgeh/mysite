7bf2e32d8c9ac7123292bc593bba9fae3937384f
hshhshsjsgeh <tri3691215@gmail.com>
2023-10-16 16:19:20 +0200

    m

--- manage.py
+++ manage.py
@@ -42,3 +42,11 @@ if __name__ == '__main__':
 # q.question_text = "What's up?"
 # q.save()
 # Question.objects.all()
+
+# from polls.models import Choice, Question
+# Question.objects.all(
+# Question.objects.filter(id=1)
+# Question.objects.filter(question_text__startswith='What')
+# from django.utils import timezone
+# current_year = timezone.now().year
+# Question.objects.get(pub_date__year=current_year)
--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,10 @@
 """
 database
 """
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -19,6 +22,8 @@ class Question(models.Model):
             return self.question_text
         except TypeError:
             return str(self.question_text)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     """the choice model
